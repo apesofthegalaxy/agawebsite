4442096eae82b6db338c9464de4a6ab000eff655
apesofthegalaxy <100108561+apesofthegalaxy@users.noreply.github.com>
2022-02-21 04:01:34 +0000

    init

---
+++ main.js
@@ -0,0 +1,40 @@
+$(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 600) {
+      $('#phase1').fadeIn();
+    } else {
+      $('#phase1').fadeOut();
+    }
+  });
+
+
+  $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 900) {
+      $('#phase2').fadeIn();
+    } else {
+      $('#phase2').fadeOut();
+    }
+  });
+
+  $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 1130) {
+      $('#phase3').fadeIn();
+    } else {
+      $('#phase3').fadeOut();
+    }
+  });
+
+
+  $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 1600) {
+      $('#phase4').fadeIn();
+    } else {
+      $('#phase4').fadeOut();
+    }
+  });
+
+
+ 
